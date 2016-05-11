beb4fbdb35d11e57f2c9e6ea40885077ebae06be
nanjusoil <nanjusoil@gmail.com>
2016-05-11 18:30:40 +0800

    完成一個方塊可以拖拉

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -9,11 +9,35 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
+	public float boxX;
+	public float boxY;
+	public int boxLength;
 
+	public boolean cursorOnBox;
+
+	public boolean boxLocked;
+
+	public float bdifx; 
+
+	public float bdify;
+	
+	private final static int width = 1200, height = 650;
 	public Character(MainApplet parent){
 
 		this.parent = parent;
+		boxX = width/2;
+
+		boxY = height/2;
 		
+		boxLength = 20;
+		
+		cursorOnBox = false;
+		
+		boxLocked = false;
+		
+		bdifx = 0;
+		
+		bdify = 0;
 	}
 
 	public void display(){
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -12,23 +12,91 @@ public class MainApplet extends PApplet{
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
 	JSONObject json;
+	Character character = new Character(this);
 	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
+		
 		size(width, height);
 		smooth();
 		loadData();
 	}
 
 	public void draw() {
+		  background(0);
 
+		  if (mouseX > character.boxX-character.boxLength && mouseX < character.boxX+character.boxLength &&  mouseY > character.boxY-character.boxLength && mouseY < character.boxY+character.boxLength) {
+
+			  character.cursorOnBox = true;  
+
+		    if(!character.boxLocked) { 
+
+		      stroke(255); 
+
+		      fill(153);
+
+		    } 
+
+		  } else {
+
+		    stroke(153);
+
+		    fill(153);
+
+		    character.cursorOnBox = false;
+
+		  }
+
+		  rect(character.boxX, character.boxY, character.boxLength, character.boxLength);
 	}
 
 	private void loadData(){
 		json = loadJSONObject(path + "starwars-episode-1-interactions.json");
 		System.out.println(json);
+	}
+
+	public void mousePressed() {
+	
+	  if(character.cursorOnBox) { 
+	
+		  character.boxLocked = true; 
+	
+	    fill(255, 255, 255);
+	
+	  } else {
+	
+		  character.boxLocked = false;
+	
+	  }
+	
+	  character.bdifx = mouseX-character.boxX; 
+	
+	  character.bdify = mouseY-character.boxY; 
+	
+	
+	
+	}
+	
+	
+	public void mouseDragged() {
+	
+	  if(character.boxLocked) {
+	
+		  character.boxX = mouseX-character.bdifx; 
+	
+		  character.boxY = mouseY-character.bdify; 
+	
+	  }
+	
+	}
+	
+	
+	
+	public void mouseReleased() {
+	
+		character.boxLocked = false;
+	  
 	}
 
 }
